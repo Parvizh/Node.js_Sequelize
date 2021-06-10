1fc2aad68a9831f39ffb7f66e6c0a18479565d93
MacBook Pro <hamzasmac@MacBooks-MacBook-Pro.local>
2021-06-10 15:15:14 +0400

    Update

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize("Rahat", "root", "", {
     dialect: "mysql",
-    host: "localhost"
+    host: "127.0.0.1"
 });
 
 const sequelizeLocal = new Sequelize("Multiple", "root", "", {
