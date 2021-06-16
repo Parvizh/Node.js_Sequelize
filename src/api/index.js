a4800c971afa57950fe30213f392f9337a70a532
Parvizh <perviz.huseynov2000@mail.ru>
2021-06-16 12:40:15 +0400

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -6,11 +6,13 @@ const post = require('../routers/post')
 const multiUser = require('../routers/multiUser');
 const likes = require('../routers/likes');
 const dle_post = require('../routers/dle_post')
+const worker = require('../routers/worker')
 
 router.use("/user",user);
 router.use("/post",post);
 router.use("/multi",multiUser);
 router.use("/like",likes);
+router.use("/worker",worker)
 router.use('/dle_post',dle_post)
 
 module.exports = router;
